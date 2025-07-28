de6cf422d578454ed852bc501c14b207f3a8e02d
emanuel <emanuelgmoll@gmail.com>
2025-07-27 23:24:05 -0300

    Cambios en el index

--- index.php
+++ index.php
@@ -2,6 +2,31 @@
 
 session_start();
 require_once __DIR__ . '../backend/controladores/usuarioController.php';
+require_once __DIR__ . '../backend/controladores/alimentoController.php';
+require_once __DIR__ . '../backend/controladores/almacenController.php';
+require_once __DIR__ . '../backend/controladores/campoController.php';
+require_once __DIR__ . '../backend/controladores/categoriaController.php';
+require_once __DIR__ . '../backend/controladores/estadoController.php';
+require_once __DIR__ . '../backend/controladores/ordenController.php';
+require_once __DIR__ . '../backend/controladores/pasturaController.php';
+require_once __DIR__ . '../backend/controladores/potreroController.php';
+require_once __DIR__ . '../backend/controladores/stock_almacenController.php';
+
+try {
+  new UsuarioController();
+  new AlimentoController();
+  new AlmacenController();
+  new CampoController();
+  new CategoriaController();
+  new EstadoController();
+  new OrdenController();
+  new PasturaController();
+  new PotreroController();
+  new Stock_almacenController();
+  // No es necesario guardar las instancias en variables si solo es para ejecutar el constructor.
+} catch (Exception $e) {
+  error_log("Error al crear tablas: " . $e->getMessage());
+}
 
 $mensaje = "";
 
@@ -18,9 +43,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if ($usuario->getRol_id() == 1) {
       header('Location: frontend/vistas/usuario/adminHome.php');
-    } elseif ($usuario->getRol_id() == 2){
+    } elseif ($usuario->getRol_id() == 2) {
       header('Location: frontend/vistasTractorista/usuario/tractoristaHome.php');
-    }else{
+    } else {
       header('Location: frontend/vistasGerencia/usuario/gerenciaHome.php');
     }
     exit;
@@ -50,7 +75,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <div class="form-title">Iniciar sesión</div>
       <form method="POST">
         <div class="form-group">
-          <input type="text" id="username" name="username"placeholder=" ">
+          <input type="text" id="username" name="username" placeholder=" ">
           <label for="username">Usuario</label>
         </div>
         <div class="form-group">
@@ -72,14 +97,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       <?php endif; ?>
     </div>
   </div>
-        <!-- <div class="content">
+  <!-- <div class="content">
                 <div class="checkbox">
                     <input type="checkbox" id="remember-me">
                     <label for="remember-me">Recordar</label>
                 </div>
                 <div class="pass-link"><a href="#">Olvide mi contraseña</a></div>
             </div> -->
-    </div>
+  </div>
   </div>
 </body>
 
